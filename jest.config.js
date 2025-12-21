7d7a6911c3076edb79788a75ff24605e06fa06fd
Satish Chandra Chitrapu <175082809+Sa007tish@users.noreply.github.com>
2025-12-22 01:41:05 +0530

    fix: configure jest to discover capability tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js"],
+  roots: ["<rootDir>/capabilities"],
+  testMatch: ["**/tests/**/*.test.ts"]
 };
