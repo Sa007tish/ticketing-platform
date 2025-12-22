283897587d703eb3e39a5c2d189bfb7af838af56
Satish Chandra Chitrapu <175082809+Sa007tish@users.noreply.github.com>
2025-12-22 19:11:53 +0530

    Update authenticateUser.test.ts

--- capabilities/capability-1-user-auth/tests/authenticateUser.test.ts
+++ capabilities/capability-1-user-auth/tests/authenticateUser.test.ts
@@ -17,7 +17,7 @@ import {
   UserNotFoundError,
   InvalidAuthenticationError,
 } from "../errors";
-import { DeterministicIdGenerator } from "../../capability-0-admin/src/idGenerator";
+import { DeterministicIdGenerator } from "../../capability-0-admin/public";
 
 const NOW = new Date("2024-01-01T00:00:00.000Z");
 
