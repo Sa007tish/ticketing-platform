afc7bf23bd105e14a814498c685388d9c1607ea6
Satish Chandra Chitrapu <175082809+Sa007tish@users.noreply.github.com>
2025-12-22 15:24:29 +0530

    Create missingInputProtection.test.ts

---
+++ capabilities/capability-1-user-auth/hardening/missingInputProtection.test.ts
@@ -0,0 +1,36 @@
+// capabilities/capability-1-user-auth/hardening/missingInputProtection.test.ts
+
+import { registerUser } from "../registerUser";
+import { authenticateUser } from "../authenticateUser";
+import { verifyAuthentication } from "../verifyAuthentication";
+import {
+  InMemoryUserStore,
+  InMemoryAuthenticationStore,
+} from "../inMemoryStores";
+import { DeterministicIdGenerator } from "../../capability-0-admin/idGenerator";
+
+test("missing or malformed input is rejected", () => {
+  const userStore = new InMemoryUserStore();
+  const authenticationStore = new InMemoryAuthenticationStore();
+  const idGenerator = new DeterministicIdGenerator([
+    "id-1",
+    "id-2",
+    "id-3",
+  ]);
+
+  // @ts-expect-error
+  expect(() => registerUser({}, userStore, idGenerator)).toThrow();
+
+  // @ts-expect-error
+  expect(() =>
+    authenticateUser(
+      { userId: "u", now: new Date() },
+      userStore,
+      authenticationStore,
+      idGenerator
+    )
+  ).toThrow();
+
+  // @ts-expect-error
+  expect(() => verifyAuthentication(undefined, authenticationStore)).toThrow();
+});
