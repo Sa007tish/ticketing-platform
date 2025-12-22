0db0e08d7f47299744c6550c825096a6aec79b5b
Satish Chandra Chitrapu <175082809+Sa007tish@users.noreply.github.com>
2025-12-22 15:23:40 +0530

    Create replayProtection.test.ts

---
+++ capabilities/capability-1-user-auth/hardening/replayProtection.test.ts
@@ -0,0 +1,47 @@
+// capabilities/capability-1-user-auth/hardening/replayProtection.test.ts
+
+import { registerUser } from "../registerUser";
+import { authenticateUser } from "../authenticateUser";
+import { verifyAuthentication } from "../verifyAuthentication";
+import {
+  InMemoryUserStore,
+  InMemoryAuthenticationStore,
+} from "../inMemoryStores";
+import { DeterministicIdGenerator } from "../../capability-0-admin/idGenerator";
+
+test("verifyAuthentication is idempotent and replay does not mutate state", () => {
+  const now = new Date("2025-01-01T00:00:00Z");
+
+  const userStore = new InMemoryUserStore();
+  const authenticationStore = new InMemoryAuthenticationStore();
+  const idGenerator = new DeterministicIdGenerator([
+    "id-1",
+    "id-2",
+    "id-3",
+  ]);
+
+  const user = registerUser({ now }, userStore, idGenerator);
+
+  const proof = authenticateUser(
+    {
+      userId: user.userId,
+      authenticationSecret: user.authenticationSecret,
+      now,
+    },
+    userStore,
+    authenticationStore,
+    idGenerator
+  );
+
+  const firstVerification = verifyAuthentication(
+    proof.proofId,
+    authenticationStore
+  );
+
+  const secondVerification = verifyAuthentication(
+    proof.proofId,
+    authenticationStore
+  );
+
+  expect(firstVerification).toEqual(secondVerification);
+});
